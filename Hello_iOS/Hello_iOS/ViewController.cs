7be8c3a23cca59928661cc2bd5c998ca94efa8fe
fitnesskimybkm <fitnesskim@naver.com>
2018-11-01 10:14:39 +0900

    file upload2

---
+++ Hello_iOS/Hello_iOS/ViewController.cs
@@ -0,0 +1,41 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace Hello_iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        //onCreate 와 같이 한번만 호출된다.
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            PhoneNumberText.ResignFirstResponder();
+
+            CallButton.TouchUpInside += (object sender, EventArgs e) =>
+            {
+                var url = new NSUrl("tel:" + PhoneNumberText.Text);
+
+                if (!UIApplication.SharedApplication.OpenUrl(url))
+                {
+                    var alert = UIAlertController.Create("Not Supported", "Schema tel: ", UIAlertControllerStyle.Alert);
+                    alert.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
+                    PresentViewController(alert, true, null);
+                }
+            };
+
+        }
+
+        public override void DidReceiveMemoryWarning ()
+        {
+            base.DidReceiveMemoryWarning ();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ iOS_Hello/iOS_Hello/ViewController.designer.cs
@@ -0,0 +1,56 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace iOS_Hello
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnAdd { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnNext { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblNames { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtName { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btnAdd != null) {
+                btnAdd.Dispose ();
+                btnAdd = null;
+            }
+
+            if (btnNext != null) {
+                btnNext.Dispose ();
+                btnNext = null;
+            }
+
+            if (lblNames != null) {
+                lblNames.Dispose ();
+                lblNames = null;
+            }
+
+            if (txtName != null) {
+                txtName.Dispose ();
+                txtName = null;
+            }
+        }
+    }
+}
